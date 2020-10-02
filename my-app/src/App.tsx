718eedfcf572d9f3a71443336a5346cfaecf4743
Drew M. Johnson <drew.m.johnson@elmutility.com>
2020-10-02 11:16:37 -0600

    Creating demo routes for application

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -10,6 +10,14 @@ const App: React.SFC<AppProps> = () => {
     <div className="Application">
       <Switch>
         <Route exact path="/" render={() => <h1>home</h1>} />
+        <Route path="/login" render={() => <h1>login</h1>} />
+        <Route path="/finance" render={() => <h1>finance</h1>} />
+        <Route path="/bucket-list" render={() => <h1>Bucket List</h1>} />
+        <Route path="/notes" render={() => <h1>notes</h1>} />
+        <Route path="/schedule" render={() => <h1>Schedule</h1>} />
+        <Route path="/reminders" render={() => <h1>Reminders</h1>} />
+        <Route path="/todo" render={() => <h1>Todo list</h1>} />
+        <Route path="/overview" render={() => <h1>Overview</h1>} />
       </Switch>
     </div>
   );
