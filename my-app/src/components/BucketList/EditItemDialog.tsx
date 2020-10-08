97a69d7b64bde39e82df4d0edeff53c3f0c549d8
Drew M. Johnson <drew.m.johnson@elmutility.com>
2020-10-08 10:44:06 -0600

    created boilerplate change handler

--- my-app/src/components/BucketList/EditItemDialog.tsx
+++ my-app/src/components/BucketList/EditItemDialog.tsx
@@ -12,24 +12,24 @@ const EditItemDialog: React.SFC<IProps> = (props: IProps) => {
     const { original, onHide } = props;
     const [edits, setEdits] = React.useState(original);
 
-    React.useEffect(() => {
-        console.log(original);
-    })
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+    }
 
     return (
         <Dialog header="Edit Item" visible={original !== null} onHide={onHide}>
             <div className="d-flex flex-column justify-content-center align-items-center">
                 {
                     edits&&Object.keys(edits).map(key => {
-                        if(key === "images"){
+                        if(key === "images" || key === "key"){
                             // ? edge case
                             return null
                         }
                         if(key === "progress"){
-                            return(<InputNumber key={key} name={key} value={edits[key]} />)
+                            return(<InputNumber key={key} name={key} value={edits[key]} onValueChange={(e: any) => handleChange(e)} />)
                         }
                         else{
-                            return( <InputText key={key} name={key} value={edits[key]} /> )
+                            return( <InputText key={key} name={key} value={edits[key]} onChange={handleChange} /> )
                         }
                     })
                 }
